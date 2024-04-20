2f1a0c5ad52592ca313ec7e98dd529eface86b23
Geovanna Mayra Domingos do Nascimento <gmdomingosn@gmail.com>
2024-04-20 10:55:52 -0300

    adiciona formulário para adição de card

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,23 @@
       <h1>Oficina de Vue.js do International Women's Day 2024</h1>
       <p>Essa é uma página criada durante a oficina do International Women's Day 2024 para praticar Vue.js.
         Aqui, mostraremos o perfil de algumas mulheres que impactaram ou impactam o ecossistema de tecnologia</p>
-      <button @click="toggleList">{{ showList ? 'Recolher cards' : 'Clique aqui para conferir' }}</button>
+      <div class="app-buttons">
+        <button @click="toggleFormOrList">{{ showList ? 'Adicionar uma mulher' : 'Ver mulheres cadastradas' }}</button>
+      </div>
     </header>
 
     <main>
       <div v-if="showList" class="profile-cards">
         <ProfileComponent v-for="woman in women" :key="woman.id" :woman="woman" />
       </div>
+      <form v-if="showForm" @submit.prevent="addWoman">
+        <input type="text" placeholder="Nome" v-model="newWoman.name" required>
+        <input type="text" placeholder="Cargo" v-model="newWoman.position" required>
+        <input type="text" placeholder="Empresa" v-model="newWoman.company" required>
+        <input type="text" placeholder="Bio" v-model="newWoman.bio" required>
+        <input type="text" placeholder="URL da foto" v-model="newWoman.photo" required>
+        <button type="submit">Adicionar</button>
+      </form>
     </main>
 
     <footer>
@@ -30,7 +40,15 @@ export default {
   },
   data() {
     return {
-      showList: false,
+      showList: true,
+      showForm: false,
+      newWoman: {
+        name: '',
+        position: '',
+        company: '',
+        bio: '',
+        photo: ''
+      },
       women: [
         { id: 1, name: 'Kizzy Terra', position: 'Professora', company: 'Inteli - Instituto de Tecnologia e Liderança', bio: 'Cientista de Dados e co-criadora do canal Programação Dinâmica no YouTube. Bacharel em Engenharia de Computação pelo Instituto Militar de Engenharia (IME). mestre em Matemática Aplicada pela FGV-RJ. e doutoranda no TIDD(PUC-SP). Além disso, trabalhei como analista de dados de utilidade pública na Fundacao Getulio Vargas e no Instituto de Pesquisa Econômica Aplicada, e fui cientista de dados sênior na Cyberlabs.', photo: 'https://wpcdn.idp.edu.br/idpsiteportal/2021/05/kizzy-terra.jpg' },
         { id: 2, name: 'Camila AChutti', position: 'CEO', company: 'Mastertech', bio: 'Camila Achutti é Fundadora da Mastertech, uma startup de educação eleita como a mais atraente do país em 2018. Criadora do portal “Mulheres na Computação”, uma das primeiras e mais importantes e influentes iniciativas de inclusão das mulheres no cenário contemporâneo de tecnologia.', photo: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQiXBYV0Q37WeRIS41nIwYyf_L9n-y8PkEnyZR542cJxZIbkFQW' },
@@ -41,8 +59,26 @@ export default {
     }
   },
   methods: {
-    toggleList() {
+    toggleFormOrList() {
       this.showList = !this.showList;
+      this.showForm = !this.showList; // Mantém apenas um dos dois verdadeiros
+    },
+    addWoman() {
+      this.women.push({
+        id: this.women.length + 1,
+        name: this.newWoman.name,
+        position: this.newWoman.position,
+        company: this.newWoman.company,
+        bio: this.newWoman.bio,
+        photo: this.newWoman.photo
+      });
+      this.newWoman = {
+        name: '',
+        position: '',
+        company: '',
+        bio: '',
+        photo: ''
+      };
     }
   }
 }
@@ -80,7 +116,6 @@ header img {
 
 main {
   flex: 1;
-  top: 0;
   padding: 100px 0;
 }
 
@@ -112,5 +147,44 @@ button:hover {
   flex-wrap: wrap;
   justify-content: center;
   gap: 20px;
+}
+
+form {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 70%;
+  /* Largura específica */
+  margin: 0 auto;
+  /* Centralização horizontal */
+  background-color: #f2f2f2;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+form input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+form button[type="submit"] {
+  width: 100%;
+  background-color: #00b698;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  font-size: 18px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+form button[type="submit"]:hover {
+  background-color: #0f7c67;
 }
 </style>
